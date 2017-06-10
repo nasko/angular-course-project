5423986faf9175205f9419511d1a1914cc5f23fb
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-06-10 12:00:07 +0300

    Render selected recipe's details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -8,13 +8,19 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://assets-jpcust.jwpsrv.com/thumbs/tjNwoclK-720.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://assets-jpcust.jwpsrv.com/thumbs/tjNwoclK-720.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://assets-jpcust.jwpsrv.com/thumbs/tjNwoclK-720.jpg'),
+    new Recipe('A Test Recipe 1', 'This is simply a test 1', 'http://assets-jpcust.jwpsrv.com/thumbs/tjNwoclK-720.jpg'),
+    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'http://assets-jpcust.jwpsrv.com/thumbs/tjNwoclK-720.jpg'),
+    new Recipe('A Test Recipe 3', 'This is simply a test 3', 'http://assets-jpcust.jwpsrv.com/thumbs/tjNwoclK-720.jpg'),
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
