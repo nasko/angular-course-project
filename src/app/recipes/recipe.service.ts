61bf594f6943e602f8c4dffa2021e5ab51ed9415
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-06-11 10:44:27 +0300

    Use RecipeService for cross-component communication and get rid of the complex chain ov event- and property binding.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe 1', 'This is simply a test 1', 'http://assets-jpcust.jwpsrv.com/thumbs/tjNwoclK-720.jpg'),
     new Recipe('A Test Recipe 2', 'This is simply a test 2', 'http://assets-jpcust.jwpsrv.com/thumbs/tjNwoclK-720.jpg'),
